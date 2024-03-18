4af5037b3aad83e5ef7abbd4aac73c5459a3d5f6
NuryanaNn <69264275+NuryanaNn@users.noreply.github.com>
2024-03-18 10:58:07 +0700

    Update AboutNav.tsx

--- components/AboutNav.tsx
+++ components/AboutNav.tsx
@@ -14,9 +14,11 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="/logo.png" alt="Company Logo" className="h-12" />
-        </a>
+        <Link href="/">
+          <a className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="/logo.png" alt="Company Logo" className="h-12" />
+          </a>
+        </Link>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
