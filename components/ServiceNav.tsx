ed94f6e98869a67d78d2a58a3c13517664ec0dbb
NuryanaNn <69264275+NuryanaNn@users.noreply.github.com>
2024-03-18 10:45:22 +0700

    Update ServiceNav.tsx

--- components/ServiceNav.tsx
+++ components/ServiceNav.tsx
@@ -1,5 +1,6 @@
 // Navbar.tsx
 import React, { useState } from 'react';
+import Link from 'next/link'; // Import Link from Next.js
 
 const Navbar: React.FC = () => {
   const [activePage, setActivePage] = useState<string>('Services');
@@ -33,23 +34,23 @@ const Navbar: React.FC = () => {
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link href="/">
-              <a className={`block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500 ${activePage === 'Home' ? 'text-gray-900 md:text-blue-700 md:p-0 dark:text-white' : 'hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-current="page" onClick={() => handlePageChange('Home')}>Home</a>
-            </Link>
+                <a className={`block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500 ${activePage === 'Home' ? 'text-gray-900 md:text-blue-700 md:p-0 dark:text-white' : 'hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-current="page" onClick={() => handlePageChange('Home')}>Home</a>
+              </Link>
             </li>
             <li>
               <Link href="/about">
-              <a className={`block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500 ${activePage === 'About' ? 'text-blue-700 md:text-blue-700 md:p-0 dark:text-white' : 'hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-current="page" onClick={() => handlePageChange('About')}>About Us</a>
+                <a className={`block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500 ${activePage === 'About' ? 'text-blue-700 md:text-blue-700 md:p-0 dark:text-white' : 'hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-current="page" onClick={() => handlePageChange('About')}>About Us</a>
               </Link>
             </li>
             <li>
               <Link href="/services">
-              <a  className={`block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500 ${activePage === 'Services' ? 'text-blue-700 md:text-blue-700 md:p-0 dark:text-white' : 'hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-current="page" onClick={() => handlePageChange('Services')}>Services</a>
+                <a  className={`block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500 ${activePage === 'Services' ? 'text-blue-700 md:text-blue-700 md:p-0 dark:text-white' : 'hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-current="page" onClick={() => handlePageChange('Services')}>Services</a>
               </Link>
             </li>
             <li>
               <Link href="/team">
-              <a href="/team" className={`block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500 ${activePage === 'Team' ? 'text-blue-700 md:text-blue-700 md:p-0 dark:text-white' : 'hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-current="page" onClick={() => handlePageChange('Team')}>Team</a>
-            </Link>
+                <a href="/team" className={`block py-2 px-3 rounded md:p-0 dark:text-white md:dark:text-blue-500 ${activePage === 'Team' ? 'text-blue-700 md:text-blue-700 md:p-0 dark:text-white' : 'hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-current="page" onClick={() => handlePageChange('Team')}>Team</a>
+              </Link>
             </li>
           </ul>
         </div>
